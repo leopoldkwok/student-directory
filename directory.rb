91ae3359d5ae8230af7270bf77c7320d587a5e8d
leopoldkwok <leopoldkwok@gmail.com>
2014-08-06 10:43:03 +0100

    Ex 8.1

--- directory.rb
+++ directory.rb
@@ -14,19 +14,23 @@ def print_footer(names)
 end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students and their cohort month"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  answer = gets.chomp.split(" ")
+  name = answer[0]
+  month = answer[1]
   # while the name is not empty, repeat this code
-  while !name.empty? do    
+  while !answer.empty? do    
     # add the student hash to the array
-    students << {:name => name, :cohort => :november}    
+    students << {:name => name, :cohort => month}    
     puts "Now we have #{students.length} students"
     # get another name from the user
-    name = gets.chomp
+    answer = gets.chomp.split(" ")
+  name = answer[0]
+  month = answer[1]
   end
   # return the array of students
   students
