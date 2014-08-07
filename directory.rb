267a185f3f4f54c2e68d9788a125811d01a23cba
leopoldkwok <leopoldkwok@gmail.com>
2014-08-07 11:08:53 +0100

    Version 9 - loading the data from file

--- directory.rb
+++ directory.rb
@@ -11,7 +11,7 @@ def print_students_list
   end
 end
 
-def print_footer  
+def print_footer    
   puts "Overall, we have #{@students.length} great students"
 end
 
@@ -23,19 +23,19 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do    
     # add the student hash to the array
-    @students << {:name => name, :cohort => :november}    
+    add_student(name, :november)
     puts "Now we have #{@students.length} students"
     # get another name from the user
     name = gets.chomp
-  end
+  end  
 end
 
-def print_menu
- # 1. print the menu and ask the user what to do
+def print_menu    
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.cv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
 end
 
 def show_students
@@ -44,30 +44,8 @@ def show_students
   print_footer
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "9"
-      exit #this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-   end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
 def save_students
-  #open the file for writing
+  # open the file for writing
   file = File.open("students.csv", "w")
   # iterate over the array of students
   @students.each do |student|
@@ -76,6 +54,43 @@ def save_students
     file.puts csv_line
   end
   file.close
+end
+
+def add_student(name, cohort)
+  @students << {:name => name, :cohort => cohort.to_sym}
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    add_student(name, cohort)
+  end
+  file.close
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students    
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def interactive_menu  
+  loop do
+    print_menu        
+    process(gets.chomp)
+  end
 end
 
 interactive_menu
